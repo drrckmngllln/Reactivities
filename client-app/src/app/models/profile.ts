2a5fff2e6494078778f8fe28babd4eaebc4d5055
Derrick Manglallan <127472849+drrckmngllln@users.noreply.github.com>
2024-06-18 14:34:11 +0800

    End of Section 20

--- client-app/src/app/models/profile.ts
+++ client-app/src/app/models/profile.ts
@@ -5,6 +5,9 @@ export interface IProfile {
     displayName: string;
     image?: string;
     bio?: string;
+    followersCount: number;
+    followingCount: number;
+    following: boolean;
     photos?: Photo[]
 }
 
@@ -14,12 +17,16 @@ export class Profile implements IProfile {
     image?: string;
     bio?: string;
     photos?: Photo[];
+    followersCount = 0;
+    followingCount = 0;
+    following = false;
 
     constructor(user: User) {
         this.username = user.username;
         this.displayName = user.displayName;
         this.image = user.image;
     }
+    
 }
 
 export interface Photo {
--- client-app/src/features/profiles/ProfilePage.tsx
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -10,10 +10,15 @@ import LoadingComponent from "../../app/layout/LoadingComponents";
 function ProfilePage() {
     const { username } = useParams<{ username: string }>();
     const { profileStore } = useStore();
-    const { loadingProfile, loadProfile, profile } = profileStore;
+    const { loadingProfile, loadProfile, profile, setActiveTab } = profileStore;
 
     useEffect(() => {
-        if (username) loadProfile(username);
+        if (username) {
+            loadProfile(username)
+            return () => {
+                setActiveTab(0)
+            }
+        };
     }, [loadProfile, username])
 
     if (loadingProfile) return <LoadingComponent content="Loading Profile..." />
